e4b6aea0b393db1728944780760d8bb4ee2b6822
libredesarrollo <desarrollolibre.net@gmail.com>
2021-10-27 16:39:42 -0400

    Expandir

--- screens/HomeScreen.vue
+++ screens/HomeScreen.vue
@@ -1,25 +1,24 @@
 <template>
-    <view>
+    <view :style="{minHeight:'100%', justifyContent:'center'}">
         <!-- <bubbles color="#F0A" /> -->
-        <input-text1 @change="change" />
+        <!-- <input-text1 @change="change" />
         <input-text1 @change="change"/>
-        <input-text2 @change="change" />
+        <input-text2 @change="change" /> -->
 
-        <ButtonModern />
-
-        <ButtonFlat>
+        <Expanded/>
 
     </view>
 </template>
 
 <script>
+import Expanded from '../components/ExpandedComponent.vue'
 import ButtonFlat from '../components/ButtonFlatComponent.vue'
 import ButtonModern from '../components/ButtonModernComponent.vue'
 import InputText1 from '../components/InputText1Component.vue'
 import InputText2 from '../components/InputText2Component.vue'
 import Bubbles from '../components/BubblesComponent.vue'
 export default {
-  components: { Bubbles, InputText1,InputText2, ButtonModern, ButtonFlat },
+  components: { Bubbles, InputText1,InputText2, ButtonModern, ButtonFlat, Expanded },
     props: {
         navigation: {
             Object
